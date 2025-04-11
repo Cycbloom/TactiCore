fa9c88919852fbe83267ca3c3fb5450d7703b0f4
Cycbloom <1756138655@qq.com>
2025-04-12 01:29:44 +0800

    chore: 更新前后端依赖和配置，添加别名
    - 更新 tsconfig 文件，添加路径别名配置
    - 统一前端代码风格，调整 import 语句格式

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,14 +1,20 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'node:path';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src')
+    }
+  },
   server: {
     port: 5173, // 允许外部访问
     host: true,
     watch: {
-      usePolling: true, // 解决容器内文件监听问题
-    },
-  },
+      usePolling: true // 解决容器内文件监听问题
+    }
+  }
 });
